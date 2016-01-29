a3e9f1e04673ea09d9b8402d53d38b6b6eeb1a82
jonwells90 <jonwells90@gmail.com>
2016-01-29 12:05:15 +0000

    Comment code

--- julia/3puzzle.jl
+++ julia/3puzzle.jl
@@ -2,8 +2,6 @@
 
 using StatsBase
 
-# Part1
-
 "Returns the 5' -> 3' antisense strand of dna_string"
 function complement_dna(dna_string)
     nucleotide_dict(x) = Dict("T"=>"A", "G"=>"C", "C"=>"G", "A"=>"T")[x]
@@ -14,19 +12,21 @@ end
 # Or a one liner:
 # replace(reverse(chomp(readall("../input/puzzle3.txt"))), r"([AGCT])", x -> Dict("T"=>"A", "G"=>"C", "C"=>"G", "A"=>"T")[x])
 
-# Part 2
-
 "Returns dictionary of fastaIDs and associated DNA sequences."
 function get_fastadict(fastafile)
-    fastadict = Dict()
+    # Open file and assert first line is correct format
     infile = open(fastafile)
+    @assert contains(readline(infile), ">") "fasta format is incorrect"
+    seekstart(infile)
+    # Initialise and build fastadict
+    fastadict = Dict()
     id = ""
     for line in readlines(infile)
         if contains(line, ">")
             id = chomp(line)
             fastadict[id] = ""
         else
-            fastadict[id] *= chomp(line)
+            fastadict[id] *= chomp(line)  # Join julia strings with * not +
         end
     end
     close(infile)
@@ -34,8 +34,8 @@ function get_fastadict(fastafile)
 end
 
 "Given fasta sequences returns sequence with highest GC content."
-function calc_maxgc_content(fastadict)
-    maxgc = 0
+function calc_maxgc_content(fastadict::Dict)
+    maxgc = 0.0
     maxid = ""
     for id in keys(fastadict)
         props = proportionmap(split(fastadict[id], ""))
@@ -45,13 +45,19 @@ function calc_maxgc_content(fastadict)
             maxid = id
         end
     end
-    return strip(maxid, '>'), 100*maxgc
+    return strip(maxid, '>'), 100.0*maxgc
 end
 
-dna = chomp(readall("../data/puzzle3a.txt"))
-dna_complement = complement_dna(dna)
-@printf("%-12s%s\n%-12s%s\n\n", "DNA:", dna, "COMPLEMENT:", dna_complement)
+function main()
+    # Part 1
+    dna = chomp(readall("../data/puzzle3a.txt"))
+    dna_complement = complement_dna(dna)
+    @printf("%-12s%s\n%-12s%s\n\n", "DNA:", dna, "COMPLEMENT:", dna_complement)
+    # Part 2
+    fastadict = get_fastadict("../data/puzzle3b.txt")
+    maxid, maxgc = calc_maxgc_content(fastadict)
+    @printf("ID:\t%s\nGC:\t%0.2f%%\n", maxid, maxgc)
+end
 
-fd = get_fastadict("../data/puzzle3b.txt")
-maxid, maxgc = calc_maxgc_content(fd)
-@printf("ID:\t%s\nGC:\t%0.2f%%\n\n", maxid, maxgc)
+main()
+
