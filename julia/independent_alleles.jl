2fc3627172735e96fd16cf62b4c0780f996017c3
Jonathan Wells <jonwells90@gmail.com>
2016-04-21 23:35:54 +0100

    Add reverse transcriptase modulo problem. Useful for keeping memory under control.

--- julia/independent_alleles.jl
+++ julia/independent_alleles.jl
@@ -1,12 +1,12 @@
 #!/usr/bin/env julia
 
-function prob_double_het(k::BigInt, n::BigInt)
+function prob_double_het(k::BigInt, n::BigInt, p::Float64)
     prob = 0
     trials = 2^k
     for r in n:trials
-        prob += binomial(trials, r)*0.25^r*0.75^(trials - r)
+        prob += binomial(trials, r)*p^r*(p-1.0)^(trials - r)
     end
     println(prob)
 end
 
-prob_double_het(BigInt(7), BigInt(37))
+prob_double_het(BigInt(7), BigInt(37), 0.25)
