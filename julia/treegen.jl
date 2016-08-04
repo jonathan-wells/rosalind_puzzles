7c9c721325b2c096fbbaa4d5cb0280aac40b5f4c
Jonathan Wells <jonwells90@gmail.com>
2016-08-04 17:51:40 +0100

    Add implementation of Heap's algorithm for permutations

--- julia/treegen.jl
+++ julia/treegen.jl
@@ -13,7 +13,9 @@
 
 type DisjointSet
     parents::Dict
+
     DisjointSet(n::Int) = new(Dict([i => Set([i]) for i in 1:n]))
+    DisjointSet(n::Set) = new(Dict([i => i for i in n]))
 end
 
 function findparent(djs::DisjointSet, x)
@@ -36,41 +38,41 @@ end
 # Just a container for a set of nodes and edges associated with them. Could add
 # lots of useful methods if desired though.
 ###############################################################################
+#
+# type Graph
+#     nodes::Set
+#     edges::Set
+# end
+#
+# function connected_components(G::Graph)
+#     subgraphs = DisjointSet(max(G.nodes...))
+#     for (n1, n2) in G.edges
+#         if findparent(subgraphs, n1) != findparent(subgraphs, n2)
+#             union!(subgraphs, n1, n2)
+#         end
+#     end
+#     return subgraphs
+# end
+#
+# function load_graph(filename)
+#     data = open(readlines, filename)
+#     nodes = Set(1:parse(Int, data[1]))
+#     edges = Set()
+#     for line in data[2:end]
+#         edge = tuple([parse(Int, i) for i in split(line)]...)
+#         push!(edges, edge)
+#     end
+#     return Graph(nodes, edges)
+# end
+#
+#
+# # Given a file describing a graph, return the number of connected subgraphs.
+# function main()
+#     G = load_graph(ARGS[1])
+#     cc = connected_components(G)
+#     numcc = length(cc.parents)
+#     println("Connected components: ", numcc)
+#     println("Edges needed to build tree: ", numcc - 1)
+# end
 
-type Graph
-    nodes::Set
-    edges::Set
-end
-
-function connected_components(G::Graph)
-    subgraphs = DisjointSet(max(G.nodes...))
-    for (n1, n2) in G.edges
-        if findparent(subgraphs, n1) != findparent(subgraphs, n2)
-            union!(subgraphs, n1, n2)
-        end
-    end
-    return subgraphs
-end
-
-function load_graph(filename)
-    data = open(readlines, filename)
-    nodes = Set(1:parse(Int, data[1]))
-    edges = Set()
-    for line in data[2:end]
-        edge = tuple([parse(Int, i) for i in split(line)]...)
-        push!(edges, edge)
-    end
-    return Graph(nodes, edges)
-end
-
-
-# Given a file describing a graph, return the number of connected subgraphs.
-function main()
-    G = load_graph(ARGS[1])
-    cc = connected_components(G)
-    numcc = length(cc.parents)
-    println("Connected components: ", numcc)
-    println("Edges needed to build tree: ", numcc - 1)
-end
-
-main()
+# main()
