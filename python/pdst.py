c66a67400e3544d449f6c2c2136549cc97569465
Jonathan Wells <jonwells90@gmail.com>
2016-10-13 16:53:38 +0100

    Add problem for generating a distance matrix (p-distance) from list of seqs of equal length.

---
+++ python/pdst.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from collections import OrderedDict
+
+def read_fasta(filename):
+    fastaseqs = OrderedDict()
+    with open(filename) as infile:
+        for line in infile:
+            if '>' in line:
+                label = line.strip()
+                fastaseqs[label] = ''
+            else:
+                fastaseqs[label] += line.strip()
+    return list(fastaseqs.values())
+
+def pdistance(s1, s2):
+    n, m = len(s1), len(s2)
+    assert n == m
+    diff = 0
+    for i in range(n):
+        if s1[i] != s2[i]:
+            diff += 1
+    return diff/n
+
+def calc_dmatrix(filename):
+    seqs = read_fasta(filename)
+    n = len(seqs)
+    dmatrix = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            dmatrix[i][j] = pdistance(seqs[i], seqs[j])
+    return dmatrix
+
+def pretty_print_matrix(filename):
+    n, m = dmatrix.shape
+    for i in range(n):
+        for j in range(n):
+            print('{0:.5f}'.format(dmatrix[i][j]), end=' ')
+        print()
+
+if __name__ == '__main__':
+    dmatrix = calc_dmatrix('../data/rosalind_pdst.txt')
+    pretty_print_matrix(dmatrix)
