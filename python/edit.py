1eb1af10a09407e9c525395736572320a2187fb7
Jonathan Wells <jonwells90@gmail.com>
2016-11-18 14:03:06 +0000

    Update gitignore,

--- python/edit.py
+++ python/edit.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from collections import OrderedDict
 
 def read_file(filename):
-    seqs = {}
+    seqs = OrderedDict()
     with open(filename) as infile:
         for line in infile:
             if '>' in line:
@@ -18,23 +19,18 @@ def levenshtein_distance(s1, s2):
     m, n = len(s1) + 1, len(s2) + 1
     mat = np.zeros((m, n), int)
     # Adds the extra indels/subs needed for prefix of diff length strings.
-    for i in range(1, m):
-        mat[i][0] = i
-    for j in range(1, n):
-        mat[0][j] = j
+    mat[:, 0] = range(m)
+    mat[0, :] = range(n)
     # pick best option of insertion, deletion or substitution where appropriate.
-    for i in range(1, m):
-        for j in range(1, n):
-            if s1[i-1] == s2[j-1]:
-                sub_cost = 0
-            else:
-                sub_cost = 1
-            mat[i][j] = min(mat[i-1][j-1] + sub_cost,
-                            mat[i-1][j] + 1,
-                            mat[i][j-1] + 1)
+    for i, c1 in enumerate(s1, 1):
+        for j, c2 in enumerate(s2, 1):
+            sub_cost = int(bool(c1 != c2))  # Neat trick: if diff sc = True = 1
+            mat[i][j] = min(mat[i-1, j-1] + sub_cost,
+                            mat[i-1, j] + 1,
+                            mat[i, j-1] + 1)
     return mat[m-1][n-1]
 
 if __name__ == '__main__':
-    seqs = read_file('../data/rosalind_edit.txt')
+    seqs = read_file('../data/rosalind_edta.txt')
     ldist = levenshtein_distance(*seqs.values())
     print(ldist)
