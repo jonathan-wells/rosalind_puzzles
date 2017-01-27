4400dc3dad5b570fa7acaf858f8ec419a247eaea
Jonathan Wells <jonwells90@gmail.com>
2017-01-27 13:40:28 +0000

    Add some brief comments to check_seqs().

--- python/corr.py
+++ python/corr.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
-import numpy as np
 
 def read_fasta(filename):
     seqs = defaultdict(str)
@@ -19,7 +18,8 @@ def hamming(s1, s2):
 def check_seqs(seqs):
     seqs = list(seqs.values())
     rev = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    revcomp = lambda s: ''.join([rev[c] for c in s[::-1]])
+    revcomp = lambda x: ''.join([rev[i] for i in x[::-1]])
+    # Count number of occurrences of sequence or its reverse complement
     seqcounts = {s: seqs.count(s) + seqs.count(revcomp(s)) for s in seqs}
     correct = [s for s in seqs if seqcounts[s] >= 2]
     incorrect = [s for s in seqs if seqcounts[s] < 2]
@@ -28,8 +28,10 @@ def check_seqs(seqs):
         for s2 in correct:
             if hamming(s1, s2) == 1:
                 corrections.append((s1, s2))
+            # This duplicates every correction for some reason...
             elif hamming(s1, revcomp(s2)) == 1:
                 corrections.append((s1, revcomp(s2)))
+    # So remove all the duplicates and sort before returning
     return sorted(set(corrections))
 
 def format_corrections(corrections):
@@ -37,7 +39,7 @@ def format_corrections(corrections):
         print(corr[0]+'->'+corr[1])
 
 def main():
-    seqs = read_fasta('../data/rosalind_corr.txt')
+    seqs = read_fasta('test.txt')
     corrections = check_seqs(seqs)
     format_corrections(corrections)
 
